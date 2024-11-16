6f18c6707d2e664cfc1ea855505002979de62b57
TuanKietTran <kyletran101.work@gmail.com>
2024-11-16 13:57:56 +0700

    to railway

--- src/gif_shop/settings.py
+++ src/gif_shop/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
 
 if DEBUG:
     ALLOWED_HOSTS += [
